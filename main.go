ff7f93a6434243ef8f1b06d2d0e664623323131d
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-07-13 16:43:16 -0300

    feat: add own logger

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/johnnyFR26/GoOpportunity/router"
+import (
+	"github.com/johnnyFR26/GoOpportunity/config"
+	"github.com/johnnyFR26/GoOpportunity/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errf("error to initialize the app: %v", err)
+		return
+	}
 	router.Initialize()
 }
