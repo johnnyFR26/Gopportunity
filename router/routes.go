d0b2709b38d12f45b6423361f899cd383635fc40
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-07-13 16:02:58 -0300

    feat: add handler to each route

--- router/routes.go
+++ router/routes.go
@@ -1,14 +1,17 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/johnnyFR26/GoOpportunity/handler"
+)
 
 func initializeRoutes(r *gin.Engine) {
 	v1 := r.Group("api/v1")
 	{
-		v1.GET("/opening")
-		v1.GET("/opening/:id")
-		v1.POST("/opening")
-		v1.PUT("/opening/:id")
-		v1.DELETE("/opening/:id")
+		v1.GET("/opening", handler.ListOpening)
+		v1.GET("/opening/:id", handler.ShowOpening)
+		v1.POST("/opening", handler.CreateOpening)
+		v1.PUT("/opening/:id", handler.UpdateOpening)
+		v1.DELETE("/opening/:id", handler.DeleteOpening)
 	}
 }
